b543ad19ee91ed7b52a4123440d2532017bb48e1
inf2005teluq <57367262+inf2005teluq@users.noreply.github.com>
2019-11-21 03:13:55 -0500

    Create Exp3_7.cpp

---
+++ Exp3_7.cpp
@@ -0,0 +1,10 @@
+include <iostream>
+int main()
+{
+char buffer[80];
+std::cout << "Entrez la chaîne : ";
+std::cin >> buffer;
+std::cout << "Contenu du tampon : " <<buffer <<    
+std::endl;
+return 0;
+}
