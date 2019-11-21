43e0e29fa1afb17c1b093f93a2ba5a8a6677e318
inf2005teluq <57367262+inf2005teluq@users.noreply.github.com>
2019-11-21 03:30:56 -0500

    Create So3_1.cpp

---
+++ So3_1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    int num = 0;
+    cout << endl << "Nombre\tCarré\tCube" << endl
+    cout << num << '\t' << num * num << '\t' << num * num * num << endl;
+    num = num + 1;cout << num << '\t' << num * num << '\t' << num * num * num << endl;
+    num = num + 1;
+    cout << num << '\t' << num * num << '\t' << num * num * num << endl;
+    num = num + 1;
+    cout << num << '\t' << num * num << '\t' << num * num * num << endl;
+    num = num + 1;
+    cout<< num << '\t' << num * num << '\t' << num * num * num << endl;
+    num = num + 1;
+    cout << num << '\t' << num * num << '\t' << num * num * num << endl;
+    num = num + 1;
+    cout << num << '\t' << num * num << '\t' << num * num * num << endl;
+    num = num + 1;
+    cout << num << '\t' << num * num << '\t' << num * num * num << endl;
+    num = num + 1;
+    cout << num << '\t' << num * num << '\t' << num * num * num << endl;
+    num = num + 1;
+    cout << num << '\t' << num * num << '\t' << num * num * num << endl;
+    num = num + 1;
+    cout << num << '\t' << num * num << '\t' << num * num * num << endl;
+return 0;
+}
