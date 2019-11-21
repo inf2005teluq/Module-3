11fbc40892e5d32b63d1ce9fca5ec71b04e0cd74
inf2005teluq <57367262+inf2005teluq@users.noreply.github.com>
2019-11-21 04:00:31 -0500

    Create So3_19.cpp

---
+++ So3_19.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int i_cube(int valeur){
+    return(valeur * valeur * valeur);
+    
+}
+    main(){cout <<"6 élevé au cube donne " << i_cube(6)<<endl;
+    cout <<"9 élevé au cube donne " << i_cube(9)<<endl;
+    cout <<"12 élevé au cube donne " << i_cube(12)<<endl;
+    return 0;
+        
+    }
