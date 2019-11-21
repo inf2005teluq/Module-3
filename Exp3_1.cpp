05f21a30a89301e6523ab2f52a67f100ea19db2f
inf2005teluq <57367262+inf2005teluq@users.noreply.github.com>
2019-11-21 02:23:09 -0500

    Create Exp3_1.cpp

---
+++ Exp3_1.cpp
@@ -0,0 +1,15 @@
+// Exemple de déclaration de fonctionsint somme (int, int); 
+// Déclaration de la fonction somme// Fonction principale qui fait appel à la fonction somme
+# include <iostream> 
+using namespace std;
+int main (void) 
+{
+int sommecalculer = somme (2,5);
+cout<<" sommecalculer = : "<< sommecalculer << endl; 
+return (0);
+// Définition de la fonction sommeint somme (int premierNombre, int deuxiemeNombre)
+{
+return (premierNombre + deuxiemeNombre); 
+// Retourne la
+// somme
+}
