97c6a29309cdd58c92658c544b9869053d2bfdf0
inf2005teluq <57367262+inf2005teluq@users.noreply.github.com>
2019-11-21 03:25:54 -0500

    Create Exp3_10.cpp

---
+++ Exp3_10.cpp
@@ -0,0 +1,13 @@
+#include < string.h > 
+#include <iostream>
+using namespace std;
+
+    int main() {
+    const int LongMax = 80;
+    char Chaine1[] = "Nul n'est prophète en son pays";
+    char Chaine2[LongMax + 1];
+    strncpy(Chaine2, Chaine1, LongMax);
+    cout << "Chaine1 : " << Chaine1 << endl;
+    cout << "Chaine2 : " << Chaine2 << endl;
+    return 0;
+  }
